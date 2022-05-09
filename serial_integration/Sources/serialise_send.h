f5bc2ac326aeca7915f2d47b976ee2ba01655f59
Lemuel Noah Fernandez <lemmyfernandez8@icloud.com>
2022-05-09 17:22:44 +1000

    Serialise integration with drivers

---
+++ serial_integration/Sources/serialise_send.h
@@ -0,0 +1,30 @@
+struct MSG_HEADER{
+  int sentinel;
+  char msg_type[8];
+  unsigned int msg_size;
+  unsigned int header_time;
+  int end_sentinel;
+};
+
+struct MSG_GYRO{
+  int sentinel;
+  int rotation_x;
+  int rotation_y;
+  int rotation_z;
+  unsigned int last_sample_time;
+};
+
+struct MSG_BUTTONS{
+  int sentinel;
+  unsigned char button_states;
+  unsigned int last_press_time;
+};
+
+void SendGyroMsg(int rot_x, int rot_y, int rot_z);
+
+void SendButtonsMsg();
+
+void SendTextMsg(char* text_message);
+
+void main_serialise(void);
+
