703f04613d0b38229683b1035b065a3d46956428
H-TCF <63900249+H-TCF@users.noreply.github.com>
2022-05-25 10:53:47 +1000

    Update Final_Project_mii_song.c

--- Final_Project_mii_song.c
+++ Final_Project_mii_song.c
@@ -3,6 +3,51 @@ void play_mii();
 
 #define numnotes 31
 #define numdurations 7
+//define each of the periods used for the notes
+
+#define rest 20
+#define Eb3 9641
+#define E3  9100
+#define F3  8589
+#define Gb3 8107
+#define G3  7652
+#define Ab3 7223
+#define A3  6817
+#define Bb3 6434
+#define B3  6073
+#define C4  5733
+#define Db4 5411
+#define D4  5107
+#define Eb4 4820
+#define E4  4550
+#define F4  4294
+#define Gb4 4053
+#define G4  3826
+#define Ab4 3611
+#define A4  3408
+#define Bb4 3611
+#define B4  3036
+#define C5  2866
+#define Db5 2705
+#define D5  2553
+#define Eb5 2410
+#define E5  2275
+#define F5  2147
+#define Gb5 2026
+#define G5  1913
+#define Ab5 1805
+#define A5  1704
+
+
+#define longnote  128      
+#define dbreve    64
+#define breve     32
+#define semibreve 16
+#define minim     8
+#define crotchet  4
+#define quaver    2
+#define semiquav  1
+
 
                                     //index's 
                                     
