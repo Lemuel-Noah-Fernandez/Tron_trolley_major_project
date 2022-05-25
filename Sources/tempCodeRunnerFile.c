794d3bef18713aafc97266a3b98a609f342c6708
danielmtro <102640424+danielmtro@users.noreply.github.com>
2022-05-25 13:35:51 +1000

    cleaning

--- Sources/tempCodeRunnerFile.c
+++
@@ -1,7 +0,0 @@
-  volatile unsigned int previous_time;
-  volatile unsigned int current_time;
-  volatile unsigned int time_diff;
-  volatile float float_time_diff;
-  volatile float distance = 0;
-  volatile float velocity = 0;
-  int count = 0;
